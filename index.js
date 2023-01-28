5070b4f5541cc1fc325c38eaeb7c83665cf2222b
FLAVIUS SERAFIM PETRU <7PETRUTU7@GMAIL.COM>
2023-01-28 21:01:04 +0200

    d

---
+++ index.js
@@ -0,0 +1,50 @@
+
+const modZi = document.getElementById("modZi")
+const divZi = document.getElementById("divZi")
+const modNoapte = document.getElementById("modNoapte")
+const divNoapte = document.getElementById("divNoapte")
+const numeCategorie = document.querySelectorAll(".numeCategorie")
+
+modNoapte.addEventListener("click", function(){
+    divNoapte.classList.add("displayOff")
+    divZi.classList.remove("displayOff")
+
+    numeCategorie.forEach((element) => {
+        element.classList.remove('textNegru');
+      });
+    
+
+numeCategorie.forEach((element) => {
+  element.classList.add('textAlb');
+});
+  
+
+
+    const body = document.querySelector("body")
+    body.classList.remove("alb")
+    body.classList.add("intunecat")
+})
+
+
+modZi.addEventListener("click", function(){
+    divZi.classList.add("displayOff")
+    divNoapte.classList.remove("displayOff")
+   
+
+    numeCategorie.forEach((element) => {
+        element.classList.remove('textAlb');
+      });
+    
+
+numeCategorie.forEach((element) => {
+  element.classList.add('textNegru');
+});
+  
+
+
+
+
+    const body = document.querySelector("body")
+    body.classList.remove("intunecat")
+    body.classList.add("alb")
+})
