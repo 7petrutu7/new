cb250906d501a9ded2ef83f91e700d12b6645c10
FLAVIUS SERAFIM PETRU <7PETRUTU7@GMAIL.COM>
2023-01-29 17:52:41 +0200

    d

--- index.js
+++ index.js
@@ -49,9 +49,48 @@ numeCategorie.forEach((element) => {
     body.classList.add("alb")
 })
 
+const inimaRosie = document.getElementById("inimaRosie")
+const inimaAlba = document.getElementById("inimaAlba")
+
+inimaAlba.addEventListener("click", function(){
+  inimaAlba.classList.add("displayOff")
+  inimaRosie.classList.remove("displayOff")
+})
+
+inimaRosie.addEventListener("click", function(){
+  inimaAlba.classList.remove("displayOff")
+  inimaRosie.classList.add("displayOff")
+})
+
+
+
+
+
+
+
+
+
+
 
 const divAnunturi = document.getElementById("divAnunturi")
 
+
+
+let count1 = 0
+let count2 = 999
 divAnunturi.addEventListener("dblclick", function(){
+  count1++
+  count2 -= 1
+  if(count1 > 0 && count2 >= 998){
+    count1--
+    
+    inimaAlba.classList.add("displayOff")
+    inimaRosie.classList.remove("displayOff")
+  }else{
+    inimaRosie.classList.add("displayOff")
+    inimaAlba.classList.remove("displayOff")
+    count2 +=2
+  }
+  
   
 })
